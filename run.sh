d711fae41c0bc0901eabb3bf3b3b9c71211ef519
Harry ZHU on Surface <zhuweixu_harry@126.com>
2019-05-06 00:40:01 +0200

    a branch for taking video, has transform and a simple demo

--- run.sh
+++ run.sh
@@ -2,5 +2,7 @@ argosBuildDir="../argos3/build"
 
 source $argosBuildDir/setup_env.sh
 
-$argosBuildDir/core/argos3 -c structure.argos
+#$argosBuildDir/core/argos3 -c structure.argos
+#$argosBuildDir/core/argos3 -c demo.argos
+$argosBuildDir/core/argos3 -c transform.argos
 
